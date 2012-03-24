fc71e2b68896a09dd9036556dee223e0e25ada8b
Morgan Grubb <morgan@wishpond.com>
2012-03-24 15:26:30 -0700

    Fallback phone

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -6,4 +6,14 @@ class Alert < ActiveRecord::Base
   validates :fallback_name, presence: true
   validates :fallback_phone, presence: true
   
+  
+  def fallback_phone=(value)
+    super(value)
+    normalise_fallback_phone
+  end
+  
+  def normalise_fallback_phone
+    self.normalised_phone_normalised = fallback_phone.gsub(/[^0-9]/, '').gsub(/^1/, '')
+  end
+  
 end
