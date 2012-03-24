4e97d17ed2aab83478b52d0896345b98adbac9f7
Godfrey Chan <kfc5@sfu.ca>
2012-03-24 11:46:01 -0700

    Empty rails 3.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngryMum::Application.config.secret_token = '3a67502cd1789b3e543a1173761b42b044a25f34f2456db7e34e9dd3737e17f13cc7cebe8705f69e2775b7c0dc6be17d25fe2f8e55295d3ab9a780fdc113e80c'
