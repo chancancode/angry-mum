9eb2c0c3783d7dec9d050cd3b8450d7f628a6589
Godfrey Chan <kfc5@sfu.ca>
2012-03-24 12:29:24 -0700

    Add caller ID to twilio init

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -5,6 +5,7 @@ begin
 
   TWILIO_ACCOUNT_SID = config['account_sid']
   TWILIO_AUTH_TOKEN  = config['auth_token']
+  TWILIO_CALLER_ID   = config['caller_id']
 rescue
   # Apparently the logger is not available in initializers, so ignoring
 end
@@ -13,3 +14,4 @@ end
 
 TWILIO_ACCOUNT_SID ||= ENV['TWILIO_ACCOUNT_SID']
 TWILIO_AUTH_TOKEN  ||= ENV['TWILIO_AUTH_TOKEN']
+TWILIO_CALLER_ID   ||= ENV['TWILIO_CALLER_ID']
