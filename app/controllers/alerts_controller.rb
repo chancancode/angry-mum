cc844885413ef1d75001f8b3cc30a670e19ce3ae
Morgan Grubb <morgan@wishpond.com>
2012-03-24 14:30:26 -0700

    Minor fixes to alerts form

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -10,6 +10,10 @@ class AlertsController < ApplicationController
     @alert = current_user.alerts.new
   end
   
+  def edit
+    @alert = current_user.alerts.find params[:id]
+  end
+  
   def create
     @alert = current_user.alerts.new(params[:alert])
     
@@ -17,6 +21,16 @@ class AlertsController < ApplicationController
       redirect_to action: 'index'
     else
       render action: 'new'
+    end
+  end
+  
+  def update
+    @alert = current_user.alerts.find params[:id]
+
+    if @alert.update_attributes params[:alert]
+      redirect_to action: 'index'
+    else
+      render action: 'edit'
     end
   end
   
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def printed_timestamp(timestamp)
+    time = timestamp.to_time
+    content_tag :span, class: 'date' do
+      if time < Time.now
+        distance_of_time_in_words(time, Time.now) + ' ago'
+      else
+        content_tag(:span, time.to_s(:short), class: "actual")
+      end
+    end
+  end
 end
